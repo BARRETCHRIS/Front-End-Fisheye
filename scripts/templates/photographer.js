0ff7e4a7aec6d1caae1b39a73ebba647171bc3e5
BARRETCHRIS <christelle.barret.poirot@gmail.com>
2023-10-17 16:02:18 +0200

    set fetch and display photographers

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,34 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, tagline, price} = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const linkPhotographer = document.createElement("a");
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const cityWrap = document.createElement("p");
+        const taglineWrap = document.createElement("p");
+        const priceWrap = document.createElement("p");   
+
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        cityWrap.textContent = city;
+        taglineWrap.textContent = tagline;
+        priceWrap.textContent = `${price} /jour`;
+
+        linkPhotographer.setAttribute("href", "#")
+        linkPhotographer.appendChild(img, h2);
+        linkPhotographer.appendChild(h2);
+
+        article.appendChild(linkPhotographer);
+        article.appendChild(cityWrap);
+        article.appendChild(taglineWrap);
+        article.appendChild(priceWrap);
+
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, tagline, price, getUserCardDOM }
 }
