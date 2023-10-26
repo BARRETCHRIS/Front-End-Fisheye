8238bcac0612a929240519a7075162159d138049
BARRETCHRIS <christelle.barret.poirot@gmail.com>
2023-10-26 10:53:12 +0200

    display photographerHeader datas by Id

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -3,15 +3,21 @@ const photographerId = urlParams.get('id');
 
 console.log(photographerId)
 
-async function getPhotographerPortfolio() {
+async function getPhotographerData(photgapherId) {
     const answer = await fetch("data/photographers.json"); 
-    const media= await answer.json();
+    const data= await answer.json();
 
-    const medias = Array.from(media.media);
-    const photographers = Array.from(media.photographers);
-    console.log(medias, photographers);
+    // const medias = Array.from(media.media);
+    // const photographers = Array.from(media.photographers);
 
-    return medias + photographers;    
+    const photographers = data.photographers.find(p => p.id === parseInt(photographerId));
+    const media = data.media.filter(m => m.photographerId === parseInt(photographerId));
+  
+
+
+    console.log(media, photographers);
+
+    return { photographers, media };    
 }
 
 function displayData(medias, photographers) {
@@ -21,9 +27,21 @@ function displayData(medias, photographers) {
 }
 
 async function init() {
-    // Récupère les datas des photographes
-    const { photographerPortfolio } = await getPhotographerPortfolio();
-    displayData(photographerPortfolio);
+    const { photographers, media } = await getPhotographerData(photographerId);
+
+    const photographTemplate = photographerGalleryTemplate(photographers);
+    const cardDOM = photographTemplate.getPhotographCardDOM();
+
+    const photographHeader = document.querySelector(".photograph-header");
+    photographHeader.appendChild(cardDOM);
+
+    const PortfolioSection = document.querySelector(".portfolio_section");
+    const gallery = document.querySelector(".gallery");
+
+    
+
+    console.log("Informations du photographe :", photographers);
+    console.log("Médias associés :", media);
 }
 
 init();
--- scripts/templates/portfolio.js
+++ scripts/templates/portfolio.js
@@ -4,17 +4,15 @@ function photographerGalleryTemplate(data){
     const picture = `assets/photographers/${portrait}`;
 
     function getPhotographCardDOM(){
-        const surnameWrap = document.createElement('div');
-        surnameWrap.classList.add(surname);
-        const portraitWrap = document.createElement('div');
-        portraitWrap.classList.add(portrait);
+        const surnameWrap = document.querySelector('.surname');
+        const portraitWrap = document.querySelector('.portrait');
         const h1 = document.createElement('h1');
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture);
         const cityWrap = document.createElement("p");
         const taglineWrap = document.createElement("p");
 
-        h2.textContent = name;
+        h1.textContent = name;
         cityWrap.textContent = city;
         taglineWrap.textContent = tagline;
 
@@ -23,6 +21,8 @@ function photographerGalleryTemplate(data){
         surnameWrap.appendChild(taglineWrap);
 
         portraitWrap.appendChild(img);
+
+
 
         return(surnameWrap + portraitWrap);    
     }
