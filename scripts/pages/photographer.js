b16151b89e0863b7851a951feb06551861a2228b
BARRETCHRIS <christelle.barret.poirot@gmail.com>
2023-10-31 23:35:14 +0100

    template gallery and adjust css

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,49 +1,15 @@
 const urlParams = new URLSearchParams(window.location.search);
 const photographerId = urlParams.get('id');
 
-// console.log(photographerId)
-
 async function getPhotographerData(photgapherId) {
     const answer = await fetch("data/photographers.json"); 
     const data= await answer.json();
 
     const photographers = data.photographers.find(p => p.id === parseInt(photographerId));
     const media = data.media.filter(m => m.photographerId === parseInt(photographerId));
-
-
-   
- 
-    //console.log(media);
-
+    
     return { photographers, media};    
 }
-
-// async function init() {
-//     const {photographers} = await getPhotographerData(photographerId);
-    
-
-//     const photographTemplate = photographerGalleryTemplate(photographers);
-//     const cardDOM = photographTemplate.getPhotographCardDOM();
-    
-
-//     const photographHeader = document.querySelector(".photograph-header");
-//     photographHeader.appendChild(cardDOM);
-
-//     const gallery = document.querySelector('.gallery');
-//     media.forEach((media) => {
-//         const mediaWrap = document.createElement('div');
-//         const img = document.createElement('img');
-//         img.setAttribute("src", `../assets/images/${photographers.name}/${media.image} || ${media.video}`)
-
-//         mediaWrap.appendChild(img);
-//         gallery.appendChild(mediaWrap);
-       
-//     });
-    
-
-//     //console.log({ photographers, media });
-
-// }
 
 async function init() {
     const { photographers, media } = await getPhotographerData(photographerId);
@@ -56,14 +22,10 @@ async function init() {
 
     const gallery = document.querySelector('.gallery');
     media.forEach((mediaItem) => {
-        const mediaWrap = document.createElement('div');
-        const img = document.createElement('img');
-        img.setAttribute("src", `../assets/images/${photographers.name}/${mediaItem.image}`);
-
-        mediaWrap.appendChild(img);
-        gallery.appendChild(mediaWrap);
-    });
+    const mediaCard = photographTemplate.getGalleryCardDOM(mediaItem);
+    gallery.appendChild(mediaCard);
+  });
 }
 
-
 init();
+
--- scripts/templates/portfolio.js
+++ scripts/templates/portfolio.js
@@ -1,6 +1,5 @@
 function photographerGalleryTemplate(data){
-    const {name, city, tagline, portrait, title, likes, image, video,} = data;
-    console.log(data);
+    const {name, city, tagline, portrait} = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getPhotographCardDOM(){
@@ -36,9 +35,39 @@ function photographerGalleryTemplate(data){
         return cardContainer;    
     }
 
-    function getGalleryCardDOM(){
+    function getGalleryCardDOM(mediaItem) {
+    const mediaWrap = document.createElement('div');
+    mediaWrap.classList.add('media-card');
 
+    const mediaInfoWrap = document.createElement('div');
+    mediaInfoWrap.classList.add('media-info');
+
+    const mediaTitle = document.createElement('h2');
+    mediaTitle.textContent = mediaItem.title;
+
+    const mediaLikes = document.createElement('p');
+    mediaLikes.innerHTML = `${mediaItem.likes} <i class="fa-solid fa-heart"></i>`;
+
+    const mediaDate = mediaItem.date;
+
+    if (mediaItem.image) {
+      const mediaImage = document.createElement('img');
+      mediaImage.setAttribute('src', `../assets/images/${name}/${mediaItem.image}`);
+      mediaWrap.appendChild(mediaImage);
+    } else if (mediaItem.video) {
+      const mediaVideo = document.createElement('video');
+      mediaVideo.setAttribute('src', `../assets/images/${name}/${mediaItem.video}`);
+      mediaVideo.setAttribute('controls', true);
+      mediaWrap.appendChild(mediaVideo);
     }
+
+    mediaInfoWrap.appendChild(mediaTitle);
+    mediaInfoWrap.appendChild(mediaLikes);
+    mediaWrap.appendChild(mediaInfoWrap);
+
+    return mediaWrap;
+  }
     
-    return {name, city, tagline, portrait, getPhotographCardDOM}
+    return { name, city, tagline, portrait, getPhotographCardDOM, getGalleryCardDOM }
+
 }
